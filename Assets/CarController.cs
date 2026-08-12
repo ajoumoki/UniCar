6c7a5684426b89f67460107c921a8e8853efb133
R.M. <bda07-s250215@gclass.ice.go.kr>
2026-08-12 21:29:36 +0900

    Update CarController.cs

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -40,7 +40,7 @@ public class CarController : MonoBehaviour
             }
             if (transform.position.x > 3) //만약 x값이 3보다 크면
             {
-                transform.Translate(-0.2f, 0, 0);
+                transform.Translate(-0.2f, 0, 0); //x값을 -0.2만큼 이동하기
             }
             if (transform.position.x < -3)
             {
