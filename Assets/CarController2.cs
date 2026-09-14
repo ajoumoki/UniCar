3326091736f7b92679aad367bc0960530c9350d0
R.M. <bda07-s250215@gclass.ice.go.kr>
2026-09-14 11:45:46 +0900

    Update CarController2.cs

--- Assets/CarController2.cs
+++ Assets/CarController2.cs
@@ -11,7 +11,7 @@ public class CarController2 : MonoBehaviour
     public Sprite RacingCar04; //스프라이트 RacingCar04 연결
     public Sprite RacingCar05; //스프라이트 RacingCar05 연결
     public Sprite RacingCar06; //스프라이트 RacingCar06 연결
-    public Sprite RacingCar07;
+    public Sprite RacingCar07; //스프라이트 RacingCar07 연결
     public Sprite RacingCar08;
     public Sprite RacingCar09;
     public Sprite RacingCar10;
